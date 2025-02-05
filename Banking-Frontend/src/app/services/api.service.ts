1abecebea611e854074f427268cc0b50d220a57a
alain-x <alainvalentin04@gmail.com>
2025-02-05 12:09:50 +0200

    initial commit

--- Banking-Frontend/src/app/services/api.service.ts
+++ Banking-Frontend/src/app/services/api.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  AuthStatusChanged = new EventEmitter<void>();
+  private static BASE_URL = 'http://localhost:8090';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+  }
+
+  registerUser(registration: any): Observable<any> {
+    return this.http.post(`${ApiService.BASE_URL}/auth/register`, registration);
+  }
+
+  loginUser(loginDetails: any): Observable<any> {
+    return this.http.post(`${ApiService.BASE_URL}/auth/login`, loginDetails);
+  }
 }
